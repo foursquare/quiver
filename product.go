e8505156acf17aa6dffe99df7952f953b0b9ca5c
David Taylor <tinystatemachine@gmail.com>
2015-08-16 03:58:08 +0000

    product of [][][]byte helper

---
+++ product.go
@@ -0,0 +1,17 @@
+package main
+
+func RevProduct(l [][][]byte) [][][]byte {
+	if len(l) == 0 {
+		return [][][]byte{nil}
+	}
+
+	head := l[0]
+	p := RevProduct(l[1:])
+	ret := make([][][]byte, 0, len(head)*len(p))
+	for _, i := range head {
+		for _, r := range p {
+			ret = append(ret, append([][]byte{i}, r...))
+		}
+	}
+	return ret
+}
---
+++ product_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func DoubleEq(a, b [][]byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, _ := range a {
+		if !bytes.Equal(a[i], b[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func TestProduct(t *testing.T) {
+	b1 := []byte{1}
+	b2 := []byte{2}
+	b3 := []byte{3}
+	b4 := []byte{4}
+
+	chunk1 := [][]byte{b1, b2}
+	chunk2 := [][]byte{b3, b4}
+
+	chunks := [][][]byte{chunk1, chunk2}
+
+	p := RevProduct(chunks)
+	if len(p) != 4 {
+		t.Logf("products: %v", p)
+		t.Fatalf("wrong number of products: %d (expected %d)", len(p), 4)
+	}
+	if !DoubleEq(p[0], [][]byte{b1, b3}) {
+		t.Fatalf("wrong product at pos 0: %v instead of %v", p[0], [][]byte{b1, b3})
+	}
+	if !DoubleEq(p[1], [][]byte{b1, b4}) {
+		t.Fatalf("wrong product at pos 1: %v instead of %v", p[1], [][]byte{b1, b4})
+	}
+	if !DoubleEq(p[2], [][]byte{b2, b3}) {
+		t.Fatalf("wrong product at pos 2: %v instead of %v", p[2], [][]byte{b2, b3})
+	}
+	if !DoubleEq(p[3], [][]byte{b2, b4}) {
+		t.Fatalf("wrong product at pos 3: %v instead of %v", p[3], [][]byte{b2, b4})
+	}
+}

f10ebb88c90152fbedf69cc388203d0ab5a6a378
David Taylor <tinystatemachine@gmail.com>
2015-09-12 22:54:44 +0000

    re-write load generator to use server-provided random keys

--- cmd/load/load.go
+++ cmd/load/load.go
@@ -2,109 +2,145 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
-	"math"
 	"math/rand"
 	"os"
 	"sort"
+	"strings"
 	"time"
 
-	"github.com/dt/go-metrics"
+	"github.com/dt/go-metrics-reporting"
 	"github.com/dt/thile/client"
 	"github.com/dt/thile/gen"
-	"github.com/foursquare/gohfile"
 )
 
-func RandomReq(name string, min, max int) *gen.SingleHFileKeyRequest {
-	count := 10
-	if false {
-		count = int(math.Abs(rand.NormFloat64())*3 + 3)
-	}
-	keys := make([]int, count)
-	lim := min + max
+type Load struct {
+	collection string
+	sample     *int64
+	keys       [][]byte
 
-	for i := 0; i < count; i++ {
-		k := rand.Intn(lim) + min
-		keys[i] = k
-	}
-	sort.Ints(keys)
+	server string
 
-	keyBytes := make([][]byte, len(keys))
-	for i, v := range keys {
-		keyBytes[i] = hfile.MockKeyInt(v)
-	}
-	return &gen.SingleHFileKeyRequest{&name, keyBytes, nil, nil}
+	work chan bool
+
+	rtt       report.Timer
+	dropped   report.Meter
+	queueSize report.Guage
 }
 
-func Send(client *gen.HFileServiceClient, r *gen.SingleHFileKeyRequest) {
-	var err error
-	_, err = client.GetValuesSingle(r)
+func (l *Load) setKeys() error {
+	c := thttp.NewThriftHttpRpcClient(l.server)
+	r := &gen.InfoRequest{&l.collection, l.sample}
 
-	if err != nil {
-		log.Println(err)
+	if resp, err := c.GetInfo(r); err != nil {
+		return err
+	} else {
+		if len(resp) < 1 || len(resp[0].RandomKeys) < 1 {
+			return fmt.Errorf("Response (len %d) contained no keys!", len(resp))
+		}
+		l.keys = resp[0].RandomKeys
+		return nil
 	}
 }
 
-func generator(work chan bool, qps int, d metrics.Meter, q metrics.Gauge) {
-	second := time.Second.Nanoseconds()
-	pause := time.Nanosecond * time.Duration(second/int64(qps))
+func (l *Load) generator(qps int) {
+	pause := time.Duration(time.Second.Nanoseconds() / int64(qps))
 
 	for _ = range time.Tick(pause) {
-		q.Update(int64(len(work)))
+		l.queueSize.Update(int64(len(l.work)))
 		select {
-		case work <- true:
+		case l.work <- true:
 		default:
-			d.Mark(1)
+			l.dropped.Mark(1)
 		}
 	}
 }
 
-func SendOne(url, name string, min, max int, m metrics.Timer) {
-	start := time.Now()
-	r := RandomReq(name, min, max)
-	c := thttp.NewThriftHttpRpcClient(url)
-	if true {
-		Send(c, r)
-	} else {
-		time.Sleep(2 * time.Millisecond)
-	}
-	m.UpdateSince(start)
+func (l *Load) sendOne(client *gen.HFileServiceClient) {
+	// TODO: randomly change up request type generated
+	l.sendSingle(client)
 }
 
-func worker(work chan bool, url, name string, min, max int, m metrics.Timer) {
-	for true {
-		<-work
-		SendOne(url, name, min, max, m)
+func (l *Load) sendSingle(client *gen.HFileServiceClient) {
+	r := &gen.SingleHFileKeyRequest{HfileName: &l.collection, SortedKeys: l.randomKeys(1)}
+	before := time.Now()
+	_, err := client.GetValuesSingle(r)
+	if err != nil {
+		log.Println("Error fetching value:", err)
+	}
+	l.rtt.UpdateSince(before)
+}
+
+func (l *Load) randomKeys(count int) [][]byte {
+	indexes := make([]int, count)
+
+	for i := 0; i < count; i++ {
+		indexes[i] = rand.Intn(len(l.keys))
+	}
+	sort.Ints(indexes)
+
+	out := make([][]byte, count)
+	for i := 0; i < count; i++ {
+		out[i] = l.keys[indexes[i]]
+	}
+	return out
+}
+
+func (l *Load) startWorkers(count int) {
+	for i := 0; i < count; i++ {
+		go func() {
+			client := thttp.NewThriftHttpRpcClient(l.server)
+			for {
+				<-l.work
+				l.sendOne(client)
+			}
+		}()
 	}
 }
 
 func main() {
-	url := flag.String("url", "http://localhost:9999/rpc/HFileService", "URL of hfile server")
-	name := flag.String("name", "testdata", "name of collection")
-	min := flag.Int("min", 0, "min int key")
-	max := flag.Int("max", -1, "max int key (required)")
+	server := flag.String("server", "localhost:9999", "URL of hfile server")
+	collection := flag.String("collection", "testdata", "name of collection")
+	graphite := report.Flag()
 	workers := flag.Int("workers", 8, "worker pool size")
-
 	qps := flag.Int("qps", 100, "qps to attempt")
+	sample := flag.Int64("sampleSize", 1000, "number of random keys to use")
 
 	flag.Parse()
 
-	if *max < 0 {
-		log.Fatal("max must be specified and >0")
+	if !strings.Contains(*server, "/") {
+		fmt.Println("URL doens't appear to specify a path -- appending /rpc/HFileService")
+		*server = *server + "/rpc/HFileService"
 	}
 
-	w := make(chan bool, (*qps)*(*workers)*3)
-	r := metrics.NewRegistry()
-	m := metrics.GetOrRegisterTimer("rtt", r)
-	d := metrics.GetOrRegisterMeter("dropped", r)
-	q := metrics.GetOrRegisterGauge("queue", r)
-
-	go metrics.LogScaled(r, time.Second*5, time.Millisecond, log.New(os.Stderr, "\t", 0))
-
-	for i := 0; i < *workers; i++ {
-		go worker(w, *url, *name, *min, *max, m)
+	if !strings.HasPrefix(*server, "http") {
+		*server = "http://" + *server
 	}
 
-	generator(w, *qps, d, q)
+	r := report.NewRecorder().
+		MaybeReportTo(graphite).
+		LogToConsole(time.Second * 10).
+		SetAsDefault()
+
+	l := &Load{
+		collection: *collection,
+		sample:     sample,
+		server:     *server,
+		work:       make(chan bool, (*qps)*(*workers)),
+		dropped:    r.GetMeter("dropped"),
+		queueSize:  r.GetGuage("queue"),
+		rtt:        r.GetTimer("rtt"),
+	}
+
+	if err := l.setKeys(); err != nil {
+		fmt.Println("Failed to fetch testing keys:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Sending %dqps to %s, drawing from %d random keys...\n", *qps, *server, len(l.keys))
+
+	l.startWorkers(*workers)
+	l.generator(*qps)
 
 }

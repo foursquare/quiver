199ae519793320d29d96936bd26e057c0a042355
David Taylor <tinystatemachine@gmail.com>
2015-10-09 20:59:55 +0000

    use generalized thrift-over-http server lib

--- http-thrift-rpc.go
+++ http-thrift-rpc.go
@@ -1,102 +1,12 @@
 package main
 
 import (
-	"net/http"
-	"sync"
-	"time"
-
-	"github.com/apache/thrift/lib/go/thrift"
 	"github.com/dt/go-metrics-reporting"
+	"github.com/dt/httpthrift"
 	"github.com/foursquare/gohfile"
 	"github.com/foursquare/quiver/gen"
 )
 
-type HttpRpcHandler struct {
-	stats   *report.Recorder
-	buffers sync.Pool
-	*gen.HFileServiceProcessor
-}
-
-func NewHttpRpcHandler(cs *hfile.CollectionSet, stats *report.Recorder) *HttpRpcHandler {
-	impl := gen.NewHFileServiceProcessor(&ThriftRpcImpl{cs})
-	return &HttpRpcHandler{stats, sync.Pool{}, impl}
-}
-
-// borrowed from generated thrift code, but with instrumentation added.
-func (p *HttpRpcHandler) Process(iprot, oprot thrift.TProtocol) (success bool, err thrift.TException) {
-	name, _, seqId, err := iprot.ReadMessageBegin()
-	if err != nil {
-		return false, err
-	}
-
-	if processor, ok := p.GetProcessorFunction(name); ok {
-		start := time.Now()
-		success, err = processor.Process(seqId, iprot, oprot)
-		if p.stats != nil {
-			p.stats.TimeSince(name, start)
-		}
-		return
-	}
-
-	iprot.Skip(thrift.STRUCT)
-	iprot.ReadMessageEnd()
-	e := thrift.NewTApplicationException(thrift.UNKNOWN_METHOD, "Unknown function "+name)
-
-	oprot.WriteMessageBegin(name, thrift.EXCEPTION, seqId)
-	e.Write(oprot)
-	oprot.WriteMessageEnd()
-	oprot.Flush()
-
-	return false, e
-}
-
-func (h *HttpRpcHandler) getBuf() *thrift.TMemoryBuffer {
-	res := h.buffers.Get()
-	if res == nil {
-		return thrift.NewTMemoryBuffer()
-	} else {
-		out := res.(*thrift.TMemoryBuffer)
-		out.Reset()
-		return out
-	}
-}
-
-func (h *HttpRpcHandler) ServeHTTP(out http.ResponseWriter, req *http.Request) {
-	if req.Method == "POST" {
-		in := h.getBuf()
-		defer h.buffers.Put(in)
-
-		in.ReadFrom(req.Body)
-		defer req.Body.Close()
-
-		compact := false
-
-		if in.Len() > 0 && in.Bytes()[0] == thrift.COMPACT_PROTOCOL_ID {
-			compact = true
-		}
-
-		outbuf := h.getBuf()
-		defer h.buffers.Put(outbuf)
-
-		var iprot thrift.TProtocol
-		var oprot thrift.TProtocol
-
-		if compact {
-			iprot = thrift.NewTCompactProtocol(in)
-			oprot = thrift.NewTCompactProtocol(outbuf)
-		} else {
-			iprot = thrift.NewTBinaryProtocol(in, true, true)
-			oprot = thrift.NewTBinaryProtocol(outbuf, true, true)
-		}
-
-		ok, err := h.Process(iprot, oprot)
-
-		if ok {
-			outbuf.WriteTo(out)
-		} else {
-			http.Error(out, err.Error(), 500)
-		}
-	} else {
-		http.Error(out, "Must POST TBinary encoded thrift RPC", 401)
-	}
+func NewHttpRpcHandler(cs *hfile.CollectionSet, stats *report.Recorder) *httpthrift.ThriftOverHTTPHandler {
+	return httpthrift.NewThriftOverHTTPHandler(gen.NewHFileServiceProcessor(&ThriftRpcImpl{cs}), stats)
 }

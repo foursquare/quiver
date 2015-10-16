efc4bf4c5f30672405526c8b6aa0534bb734bbad
David Taylor <tinystatemachine@gmail.com>
2015-10-16 14:31:54 +0000

    parameterize key counts
    also include -print-spread to print a visual representation of the distrubution of key counts

--- cmd/load/keypool.go
+++ cmd/load/keypool.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/foursquare/quiver/gen"
 	"github.com/foursquare/quiver/util"
 )
+
+func (l *Load) pickKeyCount() int {
+	return int(math.Min(math.Abs(rand.ExpFloat64()*l.keysPerReqSpread)+l.keysPerReqMin, l.keysPerReqMax))
+}
 
 // Re-fetches a new batch of keys every freq, swapping out the in-use set.
 func (l *Load) startKeyFetcher(freq time.Duration) {
@@ -37,7 +43,8 @@ func (l *Load) setKeys() error {
 	}
 }
 
-func (l *Load) randomKeys(count int) [][]byte {
+func (l *Load) randomKeys() [][]byte {
+	count := l.pickKeyCount()
 	indexes := make([]int, count)
 	l.RLock()
 	for i := 0; i < count; i++ {
@@ -57,4 +64,25 @@ func (l *Load) randomKey() []byte {
 	l.RLock()
 	defer l.RUnlock()
 	return l.keys[rand.Intn(len(l.keys))]
+}
+
+func (l *Load) printKeySpread() {
+	seen := make(map[int]int)
+	for i := 0; i < 100000; i++ {
+		k := l.pickKeyCount()
+		seen[k] = seen[k] + 1
+	}
+
+	var keys []int
+	max := 0
+	for k, v := range seen {
+		keys = append(keys, k)
+		if v > max {
+			max = v
+		}
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		fmt.Println(k, "\t", seen[k], "\t", strings.Repeat("#", seen[k]*100/max))
+	}
 }

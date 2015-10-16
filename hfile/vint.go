922122f83b7ed0d8916d002b1196a5cd2bfcec51
David Taylor <tinystatemachine@gmail.com>
2015-10-16 12:35:59 +0000

    clean up comment on vint

--- hfile/vint.go
+++ hfile/vint.go
@@ -1,23 +1,31 @@
 package hfile
 
 /*
-Attempting to make sense of the vint impl from hadoop:
+Implementation of hadoop's variable-length, signed int:
 http://grepcode.com/file/repo1.maven.org/maven2/org.apache.hadoop/hadoop-common/2.5.0/org/apache/hadoop/io/WritableUtils.java#WritableUtils.readVInt%28java.io.DataInput%29
 
-Mapping the range of possible values of the first byte byte (in java where they are for some reason signed):
-              neg*       pos*             neg                pos
-java     |-128     | -120       | -112           -1  | 0               127 |
-hex      |0x80     | 0x88       | 0x90           0xff| 0x00            0x7F|
+As far as I understand, the above-mentioned java implementation works as follows:
 
-Regions without the * are just used as-is: that is the value of the vint, and the vint is 1 byte in lenght.
-Regions with the * each contain possible 8 values, indicating how many *more* bytes make up this vint.
-After those values are read, if the count was from the "neg" region, the result is OR'ed with -1.
+A first byte in range 0 to 0x7f, or 0x90 to 0xff (ie without a * below):
+- the vint value is just the first byte, and the length is just 1.
+- NB: values between 0x90 and 0xff are negative thanks to java's signed byte type.
 
-Ordering those regions without java's silly signing:
-                  pos                 neg*      pos*           neg
-hex      | 0x00            0x7F | 0x80     | 0x88       | 0x90      0xff |
+A first byte with a value between 0x80 and 0x8F (neg* and pos* below):
+ - first byte indicates the length, and sign, of the vint.
+ - there are two ranges (0x80-0x87 and 0x88-0x8f), each with 8 possible values.
+ - logic same for both, except one indicates final value is negative.
+ - since java was treating these as signed, appears to count "down" in go.
+
+To represent possible first-byte values visually (in java's signed order first):
+java     | -128    | -120    | -112     -1  | 0             127 |
+hex      | 0x80    | 0x88    | 0x90     0xff| 0x00          0x7F|
+
+
+Putting that in an unsigned (sane) order:
+hex      | 0x00          0x7F| 0x80    | 0x88    | 0x90     0xff|
+java     | 0             127 | -128    | -120    | -112     -1  |
+meaning: | pos               | neg*    | pos*    | neg          |
 */
-
 func vintAndLen(b []byte) (int, int) {
 	first := b[0]
 	count := 1
